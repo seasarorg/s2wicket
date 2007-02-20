c1564c2207ff5d9fdf43f6231502dec6ddd65ddf
yoichiro <yoichiro@f21a5287-7e2b-49c8-ae99-86fc6edbd908>
2007-02-20 23:54:28 +0000

    パッケージを説明するJavadocを追加。 フィールド名パターンフィールドフィルタのJavaodcコメントを追記。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2wicket/trunk@48 f21a5287-7e2b-49c8-ae99-86fc6edbd908

--- s2wicket/src/main/java/org/seasar/wicket/injection/fieldfilters/FieldNamePatternFieldFilter.java
+++ s2wicket/src/main/java/org/seasar/wicket/injection/fieldfilters/FieldNamePatternFieldFilter.java
@@ -25,8 +25,68 @@ import org.seasar.wicket.injection.FieldFilter;
 
 /**
  * 与えられたフィールド名のパターンに従ってインジェクション対象のフィールドかどうかを判断するフィールドフィルタクラスです。<br />
- * <p>このフィールドフィルタを使用することにより，フィールド名の命名規則に従ってインジェクションが行われるようになります。
- * </p>
+ * <p>パッケージ名，クラス名，フィールド名について，それぞれ正規表現でパターンを与えることにより，
+ * そのパターンに一致するフィールドをインジェクション対象とするフィールドフィルタです。</p>
+ * 
+ * <p>フィールド名パターンフィールドフィルタは，@SeasarComponentアノテーションフィルタと違い，
+ * 暗黙的に適用されることはありません。フィールド名パターンフィールドフィルタを使用したい場合は，
+ * SeasarComponentInjectionListenerクラスのコンストラクタの引数で明示的に指定するか，
+ * Seasarコンテナに登録するかのどちらかの方法を使用して，使用可能な状態にする必要があります。</p>
+ *
+ * <p>フィールド名パターンフィールドフィルタは，パッケージ名，クラス名，そしてフィールド名について，
+ * それぞれ正規表現によるパターンマッチングを行います。パターンマッチングの結果，パターンに一致した
+ * 場合に，そのフィールドがインジェクション対象として判断されます。この際，Seasarコンテナから
+ * ルックアップする際のコンポーネント名は，フィールド名がそのまま使用されます。</p>
+ * 
+ * <p>このクラスは，パッケージ名，クラス名，フィールド名の正規表現文字列を持つためのプロパティが定義されています。</p>
+ *
+ * <ul>
+ * <li>パッケージ名のパターン - packageNamePatternRegex</li>
+ * <li>クラス名のパターン - classNamePatternRegex</li>
+ * <li>フィールド名のパターン - fieldNamePatternRegex</li>
+ * </ul>
+ *
+ * <p>例えば「～.exampleパッケージの～Pageクラスの～Serviceフィールドをインジェクション対象とする」
+ * という規約を適用したい場合は，以下のような正規表現を各プロパティに与えます。</p>
+ *
+ * <ul>
+ * <li>packageNamePatternRegex - .*\.example</li>
+ * <li>classNamePatternRegex - .*Page</li>
+ * <li>fieldNamePatternRegex - .*Service</li>
+ * </ul>
+ * 
+ * <p>もし複数のパターンを同時に適用したい場合は，FieldNamePatternFieldFilterオブジェクトを複数個登録してください。</p>
+ *  
+ * <p>フィールド名パターンフィールドフィルタを適用する場合は，
+ * 上記のプロパティ値として正規表現の文字列を適切にセットする必要があります。</p>
+ *
+ * <p>・明示的にインスタンスを生成する場合</p>
+ *
+ * <p>FieldNamePatternFieldFilterクラスのインスタンスを明示的に指定する場合は，
+ * 以下のようにコーディングします。</p>
+ *
+ * <pre>
+ * FieldNamePatternFieldFilter filter = new FieldNamePatternFieldFilter();
+ * filter.setPackageNamePatternRegex("パッケージ名の正規表現");
+ * filter.setClassNamePatternRegex("クラス名の正規表現");
+ * filter.setFieldNamePatternRegex("フィールド名の正規表現");
+ * </pre>
+ *
+ * <p>・Seasarコンテナに登録する場合</p>
+ *
+ * <p>このクラスのインスタンスをSeasarコンテナに登録する場合は，diconファイルに以下のように記述します。</p>
+ *
+ * <pre>
+ * &lt;component name="filter1"
+ *       class="org.seasar.wicket.injection.filters.FieldNamePatternFieldFilter"&gt;
+ *   &lt;property name="packageNamePatternRegex"&gt;"パッケージ名の正規表現"&lt;/property&gt;
+ *   &lt;property name="classNamePatternRegex"&gt;"クラス名の正規表現"&lt;/property&gt;
+ *   &lt;property name="fieldNamePatternRegex"&gt;"フィールド名の正規表現"&lt;/property&gt;
+ * &lt;/component&gt;
+ * </pre>
+ *
+ * <p>上記のコンポーネント名"field1"は例であり，任意の名前をつけることができます。</p>
+ * 
  * @author Yoichiro Tanaka
  * @since 1.2.0
  */
