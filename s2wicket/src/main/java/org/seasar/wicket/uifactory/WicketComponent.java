3738ceca31a71d62797aafca98d8398d2cfb92a7
yoichiro <yoichiro@f21a5287-7e2b-49c8-ae99-86fc6edbd908>
2007-03-29 10:11:34 +0000

    Javadocコメントに追記。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2wicket/trunk@72 f21a5287-7e2b-49c8-ae99-86fc6edbd908

--- s2wicket/src/main/java/org/seasar/wicket/uifactory/WicketComponent.java
+++ s2wicket/src/main/java/org/seasar/wicket/uifactory/WicketComponent.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.seasar.wicket.uifactory;
 
 import java.lang.annotation.Documented;
@@ -7,7 +21,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Wicketコンポーネントの生成をS2Wicketに指示することを示すアノテーションです。
+ * Wicketコンポーネントの生成をS2Wicketに指示するためのアノテーションです。<br />
+ * <p>Wicketを使用した開発では，ページを構成する複数のコンポーネントを生成し，それを親コンポーネントに登録するという，
+ * コンポーネントツリーの構築処理を記述する必要があります。これは，SwingやSWTに代表されるGUIアプリケーションにおける
+ * 画面の構築処理の記述と非常に近いものです。
+ * 
+ * 
+ * @see WicketAction
+ * @see WicketModel
  * @author Yoichiro Tanaka
  * @since 1.3.0
  */
