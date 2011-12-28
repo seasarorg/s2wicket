868edb5d9f121a55beab55b6ef9560109e1ca374
Hideyuki TAKEUCHI <chimerast@gmail.com>
2011-12-28 12:14:22 +0000

    ReloadingWicketFilterが破棄されるときクラスファイルの監視を破棄していないのをパッチ。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2wicket/trunk@221 f21a5287-7e2b-49c8-ae99-86fc6edbd908

--- s2wicket/src/main/java/org/seasar/wicket/S2WicketFilter.java
+++ s2wicket/src/main/java/org/seasar/wicket/S2WicketFilter.java
@@ -138,10 +138,12 @@ public class S2WicketFilter extends ReloadingWicketFilter {
     /** アプリケーションのデフォルトエンコーディング */
     private String applicationEncoding;
 
+    private boolean initialized = false;
+
     @Override
     public void init(final boolean isServlet, FilterConfig filterConfig)
             throws ServletException {
-        // 再読み込み時にアプリケーションが破棄されるようにする 
+        // 再読み込み時にアプリケーションが確実に破棄されているようにする 
         destroy();
 
         // コンフィギュレーションの読み取り
@@ -224,12 +226,20 @@ public class S2WicketFilter extends ReloadingWicketFilter {
                 webApplication.mountPage(debug, S2DebugPage.class);
             }
         }
+
+        initialized = true;
     }
 
     @Override
     public void destroy() {
         if (SingletonS2ContainerFactory.hasContainer()) {
             SingletonS2ContainerFactory.destroy();
+        }
+        if (initialized) {
+            ClassLoader classLoader = getClassLoader();
+            if (classLoader instanceof ReloadingClassLoader) {
+                ((ReloadingClassLoader) classLoader).destroy();
+            }
         }
         super.destroy();
     }
